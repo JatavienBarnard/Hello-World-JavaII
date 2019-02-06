7a04693500b4d6cbdce4f9b12f1dd04330505010
Jatavien Tyrese Barnard <jatavien.barnard@gmail.com>
2019-02-05 19:03:52 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 /**
  * @author Jatavien Tyrese Barnard
@@ -7,5 +8,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
