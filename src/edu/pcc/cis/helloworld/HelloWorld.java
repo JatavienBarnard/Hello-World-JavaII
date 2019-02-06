7a96ac3bd5a419409b31581d91d92c6cfb57afbd
Jatavien Tyrese Barnard <jatavien.barnard@gmail.com>
2019-02-05 19:01:40 -0800

    add main method that prints Hello World!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * @author Jatavien Tyrese Barnard
+ * @version 2/5/19
+ */
 public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
 }
